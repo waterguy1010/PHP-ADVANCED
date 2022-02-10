e4452bae3d60f8118e11162933f995e7a25aabbd
Abdel Souhali <90617360+waterguy1010@users.noreply.github.com>
2022-02-10 12:59:37 +0100

    3.3

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php
+
+echo "<ul>";
+echo "<li>artiest is ".$_GET["artiest"];
+echo "</li><li>titel is ".$_GET["titel"];
+echo "</li><li>jaar van uitkomst is ".$_GET["jaar"];
+echo "</li><li>muzieksoort is ".$_GET["muzieksoort"]."</li>";
+echo "</ul>";
+?>
+
+</body>
+</html> 
