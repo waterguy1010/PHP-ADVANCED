f5787e9b325be0b8845146ebd33d2af715616e3c
Abdel Souhali <90617360+waterguy1010@users.noreply.github.com>
2022-02-11 12:41:35 +0100

    4.1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+function schrijfkleur($kleuren) {
+    echo "<p style= 'color:$kleuren' > the color of my $kleuren</P>"
+}
+schrijfkleur('red');
+schrijfkleur('blue')''
+schrijfkleur ('yellow')
+schrijfkleur('green')
+
+
+?>
+</body>
+</html>
