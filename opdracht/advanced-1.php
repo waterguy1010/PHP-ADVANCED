1a6d5c72697283725e403db0dbc9312a877b6462
Abdel Souhali <90617360+waterguy1010@users.noreply.github.com>
2022-02-21 16:15:06 +0100

    jezus eindelijk

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -4,29 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>abdel's PHP ADVANCED EINDOPDRACHT</title>
 </head>
 <body>
-    <form name="advanced-1.php" action="advanced-2.php" method="post">
-        <select name="tekst-kleuren">
-            <?php
-                $kleuren = array("black", "blue", "green", "red", "brown");
-                foreach ($kleuren as $kleur){
-                    echo "<option value='$kleur'> $kleur </option>";
-                };
-            ?>
-        </select>
-        <select name="achtergrond-kleuren">
-            <?php
-                $kleuren = array("white","red", "blue", "green", "black", "brown");
-                foreach ($kleuren as $kleur){
-                    echo "<option value='$kleur'> $kleur </option>";
-                };
-            ?>
-        </select>
-        <input type="text" id="tabel-bord" name="tbname">
-        <input type="submit"> 
-        
-    </form>
+<center>
+ <form name="invoer" action="advanced-2.php" method="post">
+    <option>tekst-kleuren</option>
+    <select name="tekst-kleuren" id="tekst-kleuren">
+    <?php $kleuren = array ("pink", "blue", "green", "black", "brown", "red");
+    foreach($kleuren as $kleur){
+        echo "<option value = $kleur> $kleur</option>"; }
+  
+ ?>
+ </select>
+ <option>achtergrond-kleuren</option>
+ <select name="achtergrond-kleuren" id="achtergrond-kleuren">
+     <?php $kleuren = array(" brown", "blue", "green", "black", "red", "pink"); 
+     foreach($kleuren as $achtergrond_color){
+         echo "<option value = $achtergrond_color>$achtergrond_color</option>";
+     }
+     ?>
+
+ </select>  
+ <div>
+   <option>tabel-bord (px)</option>
+  <input type="text" name="tbname">
+   </div>
+    <div>
+    <option>table-padding (px)</option>
+  <input type="text" name="padding">
+    </div>
+   </div>
+    <div> <input type="submit" name="submit" value="verstuur">
+         </div>
+ <form>
+
+</center>
 </body>
 </html>
--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,37 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
-<style>
-body{
-    color:<?php echo $_POST['tekst-kleuren']?>;
-    background-color: <?php echo $_POST ['achtergrond-kleuren']?>;
+<body>
+  <style>
+    
+body {
+  color: <?= $_POST["tekst-kleuren"] ?>;
+  background-color: <?= $_POST["achtergrond-kleuren"] ?>;
 }
-table, th, td {
-  border: <?php echo $_POST ['tbname'] ?>px solid black;
-
+table,tr,td{
+    border: <?= $_POST['tbname']?>px solid black;
+    padding:<?= $_POST['padding']?>px;
 }
 </style>
-<body>
+<center>'
 
-    
+ <?php function maakRij($mijngegevens1,$value){
+     echo "<tr>
+     <td>".$mijngegevens1."</td>
+     <td>".$value."</td>
+     </tr> ";
+ }
+ ?>
+  <table>
+   <?php
+   $mijngegevens=array(
+   "naam"=> "Abdel Souhali","Leefdtijd"=> 19,"artiest"=> "Playboi Carti","woonplaats"=>"Amsterdam","sport"=> "Voetbal");
+   foreach($mijngegevens as $mijngegevens1 => $value){
+      maakRij($mijngegevens1,$value);
+   }
 
-<table>
-  <tr>
-    <?php
-     $info = array("abdel", "souhali", "kanye", "amsterdam", "voetbal");
-     foreach ($info as $alff){
-      echo "<th>$alff</th>";
-  };
-    ?>
-  </tr>
-</table>
+   ?>
+  </table>
+
+</center>
 </body>
 </html>
-
-
-
